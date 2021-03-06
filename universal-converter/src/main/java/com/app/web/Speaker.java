96509cd37ecf6a53f179b4bca89775349555655e
Krokofol <45444605+Krokofol@users.noreply.github.com>
2021-03-06 18:51:00 +0700

    400 + 404 errors

--- universal-converter/src/main/java/com/app/web/Speaker.java
+++ universal-converter/src/main/java/com/app/web/Speaker.java
@@ -2,6 +2,7 @@ package com.app.web;
 
 import com.app.Graph;
 import com.app.GraphHolder;
+import com.app.Node;
 
 import java.io.*;
 import java.net.Socket;
@@ -18,7 +19,6 @@ public class Speaker extends Thread{
     public Socket socket;
 
     public Speaker(Socket socket) {
-//        super();
         this.socket = socket;
     }
 
@@ -29,42 +29,31 @@ public class Speaker extends Thread{
             String[] units = getUnits(inputStream);
 
             if (units == null) {
+                //если у нас какие-то параметры лишние
                 return;
             }
 
             String[] fromUnit = units[0].split("/");
             String[] toUnit = units[1].split("/");
 
-            StringBuilder numeratorBuilder = new StringBuilder();
-            if (fromUnit.length > 0) numeratorBuilder.append(fromUnit[0]);
-            if (fromUnit.length > 0 && toUnit.length > 1) numeratorBuilder.append("*");
-            if (toUnit.length > 1) numeratorBuilder.append(toUnit[1]);
+            buildArgs(toUnit, fromUnit);
 
-            StringBuilder denominatorBuilder = new StringBuilder();
-            if (toUnit.length > 0) denominatorBuilder.append(toUnit[0]);
-            if (toUnit.length > 0 && fromUnit.length > 1) denominatorBuilder.append("*");
-            if (fromUnit.length > 1) denominatorBuilder.append(fromUnit[1]);
+            StringBuilder denominatorBuilder = buildArgs(fromUnit, toUnit);
+            StringBuilder numeratorBuilder = buildArgs(toUnit, fromUnit);
 
             ArrayList<String> numerator = new ArrayList<>();
             ArrayList<String> denominator = new ArrayList<>();
             Collections.addAll(numerator, numeratorBuilder.toString().replace(" ", "").split("\\*"));
             Collections.addAll(denominator, denominatorBuilder.toString().replace(" ", "").split("\\*"));
 
-            Double result = 1.0;
-//            boolean gotPare;
 
-            while (numerator.size() > 0) {
-                String numeratorIterator = numerator.get(0);
-                Graph graph = GraphHolder.findGraph(numeratorIterator);
-//                gotPare = false;
+            if (checkExistence(outputStream, numerator)) return;
+            if (checkExistence(outputStream, denominator)) return;
 
-                for (String denominatorIterator : denominator) {
-                    if(graph.existenceNode(denominatorIterator)) {
-                        result *= graph.findWay(numeratorIterator, denominatorIterator);
-                        //gotPare = true;
-                    }
-                }
-                numerator.remove(numeratorIterator);
+            Double result = calculateResult(denominator, numerator);
+            if (result == null) {
+                this.sendHeader(outputStream, "404", "Not Found", 0);
+                return;
             }
 
             byte[] answer = new DecimalFormat("#.###############").format(result).getBytes();
@@ -74,6 +63,54 @@ public class Speaker extends Thread{
         } catch (IOException error) {
             error.printStackTrace();
         }
+    }
+
+    private Double calculateResult (ArrayList<String> denominator, ArrayList<String> numerator) {
+        Double result = 1.0;
+        boolean gotPare;
+
+        if (numerator.size() != denominator.size()) {
+            return null;
+        }
+
+        while (numerator.size() > 0) {
+            String numeratorIterator = numerator.get(0);
+            Graph graph = GraphHolder.findGraph(numeratorIterator);
+            gotPare = false;
+
+            for (String denominatorIterator : denominator) {
+                if(graph.existenceNode(denominatorIterator)) {
+                    result *= graph.findWay(numeratorIterator, denominatorIterator);
+                    denominator.remove(denominatorIterator);
+                    gotPare = true;
+                    break;
+                }
+            }
+            if (!gotPare) {
+                return null;
+            }
+            numerator.remove(numeratorIterator);
+        }
+        return result;
+    }
+
+    private boolean checkExistence(OutputStream outputStream, ArrayList<String> numerator) {
+        for (String nameIterator : numerator)
+            if (!Node.checkExistence(nameIterator)) {
+                this.sendHeader(outputStream, "400", "Bad Request", 0);
+                return true;
+            }
+        return false;
+    }
+
+    private StringBuilder buildArgs(String[] fromUnit, String[] toUnit) {
+        StringBuilder denominatorBuilder = new StringBuilder();
+        if (toUnit.length > 0) denominatorBuilder.append(toUnit[0]);
+        if (toUnit.length > 0 && fromUnit.length > 1)
+            if (toUnit[0].length() > 0)
+                denominatorBuilder.append("*");
+        if (fromUnit.length > 1) denominatorBuilder.append(fromUnit[1]);
+        return denominatorBuilder;
     }
 
     private void sendHeader(OutputStream output, String statusCode, String statusText, long length) {
@@ -91,7 +128,6 @@ public class Speaker extends Thread{
             System.out.println("wrong : URL configuration");
             return null;
         }
-
 
         String[] fromTo = new String[2];
 
