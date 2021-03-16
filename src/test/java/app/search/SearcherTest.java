6fdb50594e22646550e3c27b84607c1fa3396611
Krokofol <45444605+Krokofol@users.noreply.github.com>
2021-03-16 22:13:50 +0700

    more tests +++

--- src/test/java/app/search/SearcherTest.java
+++ src/test/java/app/search/SearcherTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SearcherTest {
 
     @Test
-    public void testSearch() {
+    public void preload() {
         String[] args = {
                 "км,м,1000",
                 "м,мм,1000",
@@ -23,9 +23,32 @@ class SearcherTest {
         for (String string : args) {
             GraphHolder.parseLine(string);
         }
+    }
+
+    @Test
+    public void converting1() {
+        preload();
         String fromNodeName = "км";
         String toNodeName = "пм";
         String expectedResult = "1000000000000000";
+        Graph graph = Node.getGraph(fromNodeName);
+        Searcher searcher = new Searcher(graph, fromNodeName, toNodeName);
+        searcher.start();
+        try {
+            searcher.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String convertingResult = searcher.getResult().toString();
+        assertEquals(expectedResult, convertingResult);
+    }
+
+    @Test
+    public void converting2() {
+        preload();
+        String fromNodeName = "пм";
+        String toNodeName = "км";
+        String expectedResult = "0.000000000000001";
         Graph graph = Node.getGraph(fromNodeName);
         Searcher searcher = new Searcher(graph, fromNodeName, toNodeName);
         searcher.start();
