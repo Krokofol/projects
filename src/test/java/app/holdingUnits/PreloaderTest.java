c45eaabb1c2f7c4dc64cea0e3dc044007818f09e
Krokofol <45444605+Krokofol@users.noreply.github.com>
2021-03-24 14:11:10 +0700

    refactoring 3

--- src/test/java/app/holdingUnits/PreloaderTest.java
+++ src/test/java/app/holdingUnits/PreloaderTest.java
@@ -5,8 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class to test Preloader class.
+ *
+ * @version 1.0.0 24 Mar 2021
+ * @author Aleksey Lakhanskii
+ *
+ */
 class PreloaderTest {
 
+    /**
+     * Tests preloading. Parses test resource and checks some nodes existence.
+     */
     @Test
     public void preloadTest() {
         Preloader.preload("target/test-classes/smallTestData.csv");
